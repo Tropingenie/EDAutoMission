db3927d445800a2d4781ee08939b0d794b6af2d0
Joseff Ribble <joseffribble@gmail.com>
2022-03-15 22:42:02 -0500

    function now runs every 10 mins

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Automatically accepts missions of a type
 (This is a work in progress so expect bugs)
 
+How to use:
+1: dock and click starport services
+2: make sure mission board is selected
+3: start program
+
 Roadmap/Plans
 (Subject to change, though if its possible by my knowledge it will be added)
   - Have a UI and standalone application
--- main_ody.py
+++ main_ody.py
@@ -5,48 +5,59 @@ import cv2
 import pyautogui
 import pydirectinput
 import time
+import schedule
 
 total = 0
 
 #check for edhm as it might mess it up. i think the keybind to disable it is shift+F1 but i don't use it in ody
 #todo
 
-#run at the top of every ten mins to reduce errors
-#todo
+def checkmissions():
+    #select missions
+    pydirectinput.press('space')
+    pydirectinput.press('space')
+    time.sleep(2)
 
-#select missions
-pydirectinput.press('space')
-pydirectinput.press('space')
-time.sleep(2)
+    pydirectinput.press('d') 
+    pydirectinput.press('d')
+    pydirectinput.press('space') #changes filter to transport
+    time.sleep(5) #delay because sometimes it lags
 
-pyautogui.click(x=1090, y=475) #changes filter to transport (only works in 1920x1080)
-
-time.sleep(5)
-#main mission checking loop
-x = 0
-while x != 1:
-    try:
-        pyautogui.click('neededimages/bertselectedO.png')         #checks if image can be found on screen
-        time.sleep(1)
-        pyautogui.click('neededimages/acceptbuttonO.png')
-        total = total + 1
-        print(total + "missions so far")
-    except:
-        print('none found, moving on')
-
-    print('moving down')
-    pydirectinput.press('s')
-
-    #tell if bottom has been reached 
-    if pyautogui.pixelMatchesColor(1306, 910, (168, 73, 0)):  #this will only work on 1920x1080 displays so that must be fixed
+    #main mission checking loop
+    x = 0
+    while x != 1:
         try:
-            pyautogui.click('neededimages/bertselectedO.png')         #check one last time
+            pyautogui.click('neededimages/bertselectedO.png')         #checks if image can be found on screen
             time.sleep(1)
-            pyautogui.click(x=1434, y=882)  #also only works in 1920x1080
+            pyautogui.click('neededimages/acceptbuttonO.png')
+            total = total + 1
+            print("got one")
         except:
             print('none found, moving on')
-            x = 1
 
-#exit to refresh
-pydirectinput.press('backspace')
-pydirectinput.press('backspace')
+        print('moving down')
+        pydirectinput.press('s')
+
+        #tell if bottom has been reached 
+        if pyautogui.pixelMatchesColor(1306, 910, (168, 73, 0)):  #this will only work on 1920x1080 displays so that must be fixed
+            try:
+                pyautogui.click('neededimages/bertselectedO.png')         #check one last time
+                time.sleep(1)
+                pyautogui.click(x=1434, y=882)  #also only works in 1920x1080
+            except:
+                print('none found, moving on')
+                x = 1
+
+    #exit to refresh
+    pydirectinput.press('backspace')
+    pydirectinput.press('backspace')
+    print("done")
+    #needs check to see if missions are full
+
+#run every 10 mins when boards flip
+schedule.every(10).minutes.do(checkmissions) #Run every 10 mins (maybe change to do top of the 10 mins so it doesn't break during a flip)
+schedule.run_all() #start now
+
+while True:  #main loop
+    schedule.run_pending() #scheduler has a pending function
+    time.sleep(1) #check every 1 seconds
