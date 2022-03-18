95c2c94bc91c815f73e991df365099393273caeb
noobboy3064 <eugenelm05@outlook..com>
2022-03-18 14:40:23 +0800

    Added menu navigation
    What do you expect lmao. Navigates the menu.

--- OCRHorizons.py
+++ OCRHorizons.py
@@ -9,10 +9,8 @@ import logging
 import time
 
 xdo = Xdo()
-log = logging.getLogger("log")
-
- os.remove("temp_screenshot.png")#Remember to put it back
-
+log = logging.getLogger("__name__")
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "NOTSET"))
 
 
 def parse_selected_mission():
@@ -24,23 +22,65 @@ def parse_selected_mission():
     """
     # Locate the mission that is selected and take a screenshot of it.
     # Confidence is low so as to catch all missions.
-    selected = pyautogui.locateOnScreen("neededimages/mission.png",
-                                        confidence=0.1)
-    pyautogui.screenshot("temp_screenshot.png", region=selected)
-
+    selected = pyautogui.locateOnScreen("neededimages/mission.png",confidence=0.4)
+    im = pyautogui.screenshot("temp_screenshot.png", region=selected)
+    im1 = Image.open("comp_shot.png")
+    im2 = Image.open("temp_screenshot.png")
+    
+    if list(im1.getdata()) == list(im2.getdata()):
+        break  
+    
     # Run the screenshot through OCR and save it to a variable
-    text = pytesseract.image_to_string(
-        np.array(Image.open("temp_screenshot.png"))
-        )
-      # Delete the temp file
+    text = pytesseract.image_to_string(np.array(Image.open("temp_screenshot.png")))
+    im.save("comp_shot.png")
+    os.remove("temp_screenshot.png")# Delete the temp file
 
     logging.debug(text)
-    return text, selected
+    logging.debug(selected)
+    pyautogui.press("up")
+    return text
 
+def setup(m, faction): 
+    if m == 1:
+        pyautogui.press("left", presses=2, interval=0.5)
+        pyautogui.press("down", presses=5, interval=0.5)#Set mission type to transport, not set already
+        pyautogui.press("up", presses=7, interval=0.5)
+        pyautogui.press("space", presses=1, interval=0.5)
+        pyautogui.press("down", presses=4, interval=0.5)
+        pyautogui.press("space", presses=1, interval=0.5)
+        
+        pyautogui.press("left", presses=4, interval=0.5)
+        pyautogui.press("down", presses=7, interval=0.5)#Reset to origin, which the the back button
+        logging.debug("Set mode to transport, returned to origin")
+        print("done") 
+    if faction == "rook":
+        pyautogui.press("left", presses=3 ,interval=0.3)
+        pyautogui.press("down", presses=8 ,interval=0.3)
+        pyautogui.press("up", presses=5, interval=0.5)
+        pyautogui.press("down", presses=1, interval=0.5)   
+        pyautogui.press("right")
+        logging.debug("Set faction to Rook Platoon")
+        
+    if faction == "gus fring":
+        pyautogui.press("left", presses=3 ,interval=0.3)
+        pyautogui.press("down", presses=8 ,interval=0.3)
+        pyautogui.press("up", presses=5, interval=0.5)
+        pyautogui.press("down", presses=2, interval=0.5)   
+        pyautogui.press("right")
+        logging.debug("Set faction to Bureau of Apotanites Flag")
+    if faction == "coop":
+        pyautogui.press("left", presses=3 ,interval=0.3)
+        pyautogui.press("down", presses=8 ,interval=0.3)
+        pyautogui.press("up", presses=5, interval=0.5)
+        pyautogui.press("down", presses=3, interval=0.5)   
+        pyautogui.press("right")
+        
+    else:
+        logging.debug("No faction chosen")
+        
 
-
-
-win_id = xdo.search_windows(pid = 15347)
-print(win_id)
-time.sleep(10)
-parse_selected_mission()
+#win_id = xdo.search_windows(winname="EliteDangerous6")
+time.sleep(10) 
+#print(parse_selected_mission())
+setup(1,"rook")
+print(time.asctime() + " Mission Set")
